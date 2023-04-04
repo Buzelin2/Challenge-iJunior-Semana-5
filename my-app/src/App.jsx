a62a26f6929dbf4e39703f6bd11bbd15bc2f0b31
Arthur Buzelin <buzelin16@gmail.com>
2023-04-04 16:18:28 -0300

    Criação divs principais e espaço da bill

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,9 +6,22 @@ function App() {
 
   return (
    <div className='tudo'>
-    <p className='split'>SPLI</p>
-    <p className='split'>TTER</p>
-    <div className='main'></div>
+    <div className='splitter'>
+      <p className='split'>SPLI</p>
+      <p className='split'>TTER</p>
+    </div>
+    <div className='main'>
+      <div className='left'>
+        <div className='input_money'>
+          <p className='bill'>Bill</p>
+          <input class="money" type="text" placeholder="0"></input>
+        </div>
+
+      </div>
+      <div className='right'>
+        
+      </div>
+    </div>
    </div>
 )}
 
