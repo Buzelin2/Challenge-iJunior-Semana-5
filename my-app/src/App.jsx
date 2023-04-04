cc82294adaac735e611bcfa447317ef54efabda5
Arthur Buzelin <buzelin16@gmail.com>
2023-04-04 18:06:12 -0300

    Div left quase completa

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -14,7 +14,24 @@ function App() {
       <div className='left'>
         <div className='input_money'>
           <p className='bill'>Bill</p>
-          <input class="money" type="text" placeholder="0"></input>
+          <input class="count" type="text" placeholder="0"></input>
+        </div>
+        <p className='tip'>Select Tip %</p>
+        <div className='botoes'>
+          <div className='botoes_cima'>
+            <button className='botao'>5%</button>
+            <button className='botao'>10%</button>
+            <button className='botao'>15%</button>
+          </div>
+          <div className='botoes_baixo'>
+            <button className='botao'>25%</button>
+            <button className='botao'>50%</button>
+            <button className='botao botao6'>Custom</button>
+          </div>
+        </div>
+        <div className='input_money'>
+          <p className='num_people'>Number of people</p>
+          <input class="count" type="text" placeholder="0"></input>
         </div>
 
       </div>
